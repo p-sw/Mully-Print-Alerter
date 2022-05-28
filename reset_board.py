263b0517c36c42383b09e1a84280e89423708140
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-28 07:21:35 +0000

    Added table reset util script

---
+++ reset_board.py
@@ -0,0 +1,15 @@
+import psycopg2 as pg
+from settings import DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT
+
+db = pg.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASS,
+    port=DB_PORT
+)
+
+cursor = db.cursor()
+cursor.execute('DELETE FROM board_history')
+db.commit()
+print('JOB DONE.')
