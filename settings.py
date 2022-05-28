eb3e351ef172bc2dce61ff8fd0d65bef7dc9c947
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-28 06:58:32 +0000

    Changed DB URL to postgre default env_var

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@ from urllib.parse import urlparse
 target_url = "http://bupyeong.icehs.kr/boardCnts/list.do?boardID=290173&m=0903&s=bupyeong"
 guild_id=["939031979893088328", "979944212051148813"]
 
-DB_URI = os.environ.get('DB_URI')
-PARSED_URI = urlparse(DB_URI)
-DB_HOST = PARSED_URI.hostname
-DB_NAME = PARSED_URI.path[1:]
-DB_USER = PARSED_URI.username
-DB_PASS = PARSED_URI.password
-DB_PORT = PARSED_URI.port
+DB_URL = os.environ.get('DATABASE_URL')
+PARSED_URL = urlparse(DB_URL)
+DB_HOST = PARSED_URL.hostname
+DB_NAME = PARSED_URL.path[1:]
+DB_USER = PARSED_URL.username
+DB_PASS = PARSED_URL.password
+DB_PORT = PARSED_URL.port
